22c1094caad722a70a153729ef092d92b61bd697
Emir Hamza Paspal <hamzatospas@gmail.com>
2025-05-18 16:27:47 +0300

    Changed The Title

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import TodoForm from "./Todos";
 export default function Home() {
   return (
     <div>
-      <h1 className="mx-auto w-fit text-3xl font-bold m-5">Hello world!</h1>
+      <h1 className="mx-auto w-fit text-3xl font-bold m-5">TODO App</h1>
 
       <TodoForm/>
     </div>
